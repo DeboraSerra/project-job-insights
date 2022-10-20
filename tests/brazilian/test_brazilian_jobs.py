1dcb93dafcf9673919cadd2eaa09bf0873ddc9ce
debora <debora.r.serra@gmail.com>
2022-10-20 17:22:46 -0300

    feat: add req11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for job in result:
+        assert job['title'] is not None
+        assert job['salary'] is not None
+        assert job['type'] is not None
+        assert job['titulo'] is None
+        assert job['salario'] is None
+        assert job['tipo'] is None
