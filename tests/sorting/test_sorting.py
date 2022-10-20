01d8f443697bd31189260675a6201bcf9c9a67d8
debora <debora.r.serra@gmail.com>
2022-10-20 18:03:04 -0300

    fix: fix lint

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -4,9 +4,16 @@ from src.jobs import read
 
 
 def test_sort_by_criteria():
-    result_min_salary = sort_by(read('tests/mocks/jobs.csv'), 'min_salary')
-    result_max_salary = sort_by(read('tests/mocks/jobs.csv'), 'max_salary')
-    result_date = sort_by(read('tests/mocks/jobs.csv'), 'date_posted')
-    assert result_min_salary[0]['min_salary'] < result_min_salary[1]['min_salary']
-    assert result_max_salary[0]['max_salary'] > result_max_salary[1]['max_salary']
-    assert date(result_date[0]['date_posted']) > date(result_date[1]['date_posted'])
+    dict_file = read("src/jobs.csv")
+    result_min_salary = sort_by(dict_file, "min_salary")
+    result_max_salary = sort_by(dict_file, "max_salary")
+    result_date = sort_by(dict_file, "date_posted")
+    date1 = result_date[0]["date_posted"]
+    date2 = result_date[1]["date_posted"]
+    assert int(
+        result_min_salary[0]["min_salary"] < result_min_salary[1]["min_salary"]
+    )
+    assert int(
+        result_max_salary[0]["max_salary"] > result_max_salary[1]["max_salary"]
+    )
+    assert date.fromisoformat(date1) > date.fromisoformat(date2)
