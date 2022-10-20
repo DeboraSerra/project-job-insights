d5cf1d47130a970081b5a2c3363c3c9fbc554c5f
debora <debora.r.serra@gmail.com>
2022-10-20 17:39:55 -0300

    fix: fix req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -4,9 +4,9 @@ from src.brazilian_jobs import read_brazilian_file
 def test_brazilian_jobs():
     result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
     for job in result:
-        assert job['title'] is not None
-        assert job['salary'] is not None
-        assert job['type'] is not None
-        assert job['titulo'] is None
-        assert job['salario'] is None
-        assert job['tipo'] is None
+        assert 'title' in job
+        assert 'salary' in job
+        assert 'type' in job
+        assert 'titulo' not in job
+        assert 'salario' not in job
+        assert 'tipo' not in job
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,12 @@
-# from src.sorting import sort_by
+from datetime import date
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    result_min_salary = sort_by(read('tests/mocks/jobs.csv'), 'min_salary')
+    result_max_salary = sort_by(read('tests/mocks/jobs.csv'), 'max_salary')
+    result_date = sort_by(read('tests/mocks/jobs.csv'), 'date_posted')
+    assert result_min_salary[0]['min_salary'] < result_min_salary[1]['min_salary']
+    assert result_max_salary[0]['max_salary'] > result_max_salary[1]['max_salary']
+    assert date(result_date[0]['date_posted']) > date(result_date[1]['date_posted'])
